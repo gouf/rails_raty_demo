cfad9364f96d84490b04f5b2e555f75e4643a05d
gouf <innocent.zero@gmail.com>
2018-02-04 23:43:47 +0900

    Implement rating UI using raty-js

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,2 +1,27 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+// jQuery Plugin. Set/show rating by star
+function initRaty() {
+  if($('.raty-read').length) {
+    $('.raty-read').raty({
+      starType: 'i',
+      space: false,
+      readOnly: true,
+      score: function() { return $(this).data('rating') }
+    })
+  }
+
+  if($('.raty').length) {
+    $('.raty').raty({
+      starType: 'i',
+      target: '#review_rate', // Sync rating to #review_rate
+      targetKeep: true,
+      targetType: 'number',
+      space: true,
+      score: function() { return $(this).data('rating') }
+    })
+  }
+}
+
+$(document).on('load turbolinks:load', initRaty)
